005d0f5651dc74dc103c5545ec34afbb6f6e2fd6
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2023-08-06 17:08:03 +0000

    stage 5

--- index.js
+++ index.js
@@ -106,9 +106,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    const underFundedGames = GAMES_JSON.filter((games) =>{
+        return games.goal < games.pledged;
+    });
 
     // use the function we previously created to add the unfunded games to the DOM
+    addGamesToPage(underFundedGames);
 
 }
 
@@ -117,9 +120,13 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
+    const fundedGames = GAMES_JSON.filter((games) =>{
+        return games.pledged >= games.goal;
+    });
 
 
     // use the function we previously created to add unfunded games to the DOM
+    addGamesToPage(fundedGames);
 
 }
 
@@ -128,6 +135,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
 
 }
 
@@ -137,6 +145,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly());
+fundedBtn.addEventListener("click", filterFundedOnly());
+allBtnBtn.addEventListener("click", showAllGames());
 
 
 /*************************************************************************************
